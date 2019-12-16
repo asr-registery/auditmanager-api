fa141926434d1655335e70b82090d97e6aecffa9
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-16 11:15:08 +0430

    init cnf

---
+++ src/test/java/af/asr/auditmanagerapi/AuditmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.auditmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
