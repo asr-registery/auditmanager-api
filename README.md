475b2484f1d4404cccf3dedf46647833bec72d82
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-16 11:18:49 +0430

    init cnf

---
+++ README.md
@@ -0,0 +1,8 @@
+### Audit Management Library 
+
+#### Features
+
+- User Actions Audit API
+- Exception Handling
+- Audit Report Access API 
+- 
---
+++ src/main/java/af/asr/auditmanagerapi/util/AuditUtils.java
@@ -0,0 +1,45 @@
+package af.asr.auditmanagerapi.util;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+/**
+ * Utility class for Audit Manager
+ *
+ */
+public class AuditUtils {
+
+	/**
+	 * Private constructor for AuditUtils
+	 */
+	private AuditUtils() {
+
+	}
+
+	/**
+	 * Function to validate {@link AuditRequestDto}
+	 * 
+	 * @param auditRequest The audit request
+	 */
+	public static void validateAuditRequest(AuditRequestDto auditRequest) {
+		ValidatorFactory factory = null;
+		try {
+			factory = Validation.buildDefaultValidatorFactory();
+			Validator validator = factory.getValidator();
+
+			Set<ConstraintViolation<AuditRequestDto>> violations = validator.validate(auditRequest);
+
+			if (!violations.isEmpty()) {
+				throw new AuditManagerException(AuditErrorCodes.HANDLEREXCEPTION.getErrorCode(),
+						AuditErrorCodes.HANDLEREXCEPTION.getErrorMessage());
+			}
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+	}
+}
